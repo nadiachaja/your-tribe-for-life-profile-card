355ca8b45c8c586b301b8dbbf2479690d0031ebe
nadiachaja <nadiagaja13@gmail.com>
2025-09-03 15:10:49 +0200

    code goede zetten

--- myapp/src/routes/+page.server.js
+++ myapp/src/routes/+page.server.js
@@ -1,6 +1,6 @@
 export async function load({url}){
     const membersResponse= await fetch('https://fdnd.directus.app/items/person/?sort=name&fields=*,squads.squad_id.name,squads.squad_id.cohort&filter={%22_and%22:[{%22squads%22:{%22squad_id%22:{%22tribe%22:{%22name%22:%22FDND%20Jaar%202%22}}}},{%22squads%22:{%22squad_id%22:{%22cohort%22:%222526%22}}}]}')
-const membersData = await membersResponse.json();
+    const membersData = await membersResponse.json();
 
-return {members:membersData.data};
+    return {members:membersData.data};
 }
--- myapp/src/routes/[id]/+page.server.js
+++ myapp/src/routes/[id]/+page.server.js
@@ -1,6 +1,6 @@
 export async function load({url, params}){
     const memberResponse= await fetch('https://fdnd.directus.app/items/person/'+ params.id)
-const memberData = await memberResponse.json();
+    const memberData = await memberResponse.json();
 
-return {member:membersData.data};
+    return { member:membersData.data };
 }
