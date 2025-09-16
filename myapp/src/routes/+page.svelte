a7d50b2be83b1198ae9135804c1525db50f47713
nadiachaja <nadiagaja13@gmail.com>
2025-09-16 10:51:47 +0200

    #5 mugshot

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -5,22 +5,19 @@ import { Social } from '$lib'
 import cat from '$lib/assets/cat.svg';
 </script>
 
-<div class="member-image-wrapper">
-    <img class="profile-picture" src={`https://fdnd.directus.app/assets/${member.mugshot}`} width="300" height="200" alt={member.name} />
-</div>
-
 <section>
     <h1>{member.name}</h1>
-
     <p>{member.bio}</p>
 </section>
 
-
-<div>
+<div class="social-wrapper">
 <Social icon="github" url="#" name="github" />
 <Social icon="linkedin" url="#" name="linkedin" />
+</div>
 
-<img src={cat} width="100" height="150" alt="cat">
+<div class="profile-wrapper">
+    <img class="profile-picture" src={`https://fdnd.directus.app/assets/${member.mugshot}`} width="300" height="200" alt={member.name} />
+    <img class="cat-image" src={cat} width="100" height="150" alt="cat">
 </div>
 
 <!-- <a href="/{member.id}">{member.name} - {member.id}</a> -->
