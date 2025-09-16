dfafa1dce307970546ae43c5fabf867561616703
nadiachaja <nadiagaja13@gmail.com>
2025-09-16 11:10:19 +0200

    #6 #7 styleguide in layout.svelte

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
@@ -9,15 +9,22 @@
 </svelte:head>
 
 <main>
-{@render children?.()}
+	{@render children?.()}
 </main>
 
 <!-- Hier kan je zetten wat hetzelfde op alle pagina's moet -->
 <!-- Hier kan mijn header, nav, menu en footer -->
- 
 
 <style>
-	/* styleguide */
+	:root {
+		--primary-bg:#84a1bd;
+		--primary-text:#000000;
 
+		--border-radius-m:10px;
+		--border-radius-mixed:10px 0px 0px 0px;
 
+		--font-size-m: clamp(1.75rem, 1.5rem + 1.25vw, 3rem);
+		--font-size-s: clamp(1rem, 0.95rem + 0.25vw, 1.25rem);
+
+	}
 </style>
